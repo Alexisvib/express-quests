8037a4aed8e0022316ba44579df621ea78dc653f
Alexis VIBERT <avibert@omnilog.fr>
2021-10-01 16:16:42 +0200

    quest_6

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // Express is already installed
+const { json } = require("express");
 const express = require("express");
 const db = require("./db-config");
 
@@ -20,21 +21,55 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/movies", async (req, res) => {
-  try {
-    const rows = await db.promise().query("SELECT * FROM movies");
-    return res.status(200).json(rows[0]);
-  } catch (err) {
-    res.status(500).send("Error retrieving data from database");
+  let sqlQuery = "SELECT * FROM movies";
+
+  const color = req.query.color;
+
+  const max_duration = req.query.max_duration;
+
+  const sqlValues = [];
+
+  if (color && !max_duration) {
+    sqlQuery += " WHERE color = ?";
+    sqlValues.push(color);
   }
+
+  if (max_duration && !color) {
+    sqlQuery += " WHERE duration <= ?";
+    sqlValues.push(max_duration);
+  }
+
+  if (max_duration && color) {
+    sqlQuery += " WHERE color= ? AND duration <= ?";
+    sqlValues.push(color, max_duration);
+  }
+
+  db.query(sqlQuery, sqlValues, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.status(200).json(results);
+    }
+  });
 });
 
 app.get("/api/movies/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const movie = movies.find((movie) => movie.id === id);
-  if (!movie) {
-    res.status(404).send("Not Found");
-  }
-  res.status(200).json(movie);
+  db.query(
+    "SELECT * FROM movies WHERE id = ?",
+    [req.params.id],
+    (err, result) => {
+      if (err) {
+        return res.status(500).send("Error getting the movie");
+      } else {
+        if (result.length > 0) {
+          return res.status(200).json(result[0]);
+        } else {
+          return res.status(404).send("Movie not found");
+        }
+      }
+    }
+  );
 });
 
 app.get("/api/search", (req, res) => {
@@ -47,7 +82,21 @@ app.get("/api/search", (req, res) => {
 });
 
 app.get("/api/users", (req, res) => {
-  res.status(401).send("unauthorized");
+  let sqlQuery = "SELECT * FROM users";
+  const sqlValues = [];
+  if (req.query.language) {
+    sqlQuery += " WHERE language = ?";
+    sqlValues.push(req.query.language);
+  }
+
+  db.query(sqlQuery, sqlValues, (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error getting the users");
+    } else {
+      return res.status(200).json(result);
+    }
+  });
 });
 
 app.post("/api/movies", (req, res) => {
@@ -69,7 +118,7 @@ app.post("/api/movies", (req, res) => {
 app.post("/api/users", (req, res) => {
   const { firstname, lastname, email } = req.body;
   db.query(
-    "INSERT INTO user (firstname, lastname, email) VALUES (?,?,?)",
+    "INSERT INTO users (firstname, lastname, email) VALUES (?,?,?)",
     [firstname, lastname, email],
     (err, result) => {
       if (err) {
@@ -102,7 +151,7 @@ app.put("/api/users/:id", (req, res) => {
 
 app.delete("/api/users/:id", (req, res) => {
   const userId = req.params.id;
-  db.query("DELETE FROM user WHERE id = ?", [userId], (err, result) => {
+  db.query("DELETE FROM users WHERE id = ?", [userId], (err, result) => {
     if (err) {
       res.status(500).send("Error deleting the user");
     } else {
@@ -111,17 +160,31 @@ app.delete("/api/users/:id", (req, res) => {
   });
 });
 
+app.get("/api/users/:id", (req, res) => {
+  user = req.params.id;
+  db.query("SELECT * FROM users WHERE id = ?", [user], (err, result) => {
+    if (err) {
+      res.status(500).send("Error during the process of getting the user");
+    } else {
+      if (result.length > 0) {
+        res.status(200).json(result[0]);
+      } else {
+        res.status(200).send("No user corresponding to the id provided");
+      }
+    }
+  });
+});
 
 app.delete("/api/movies/:id", (req, res) => {
-    const movieId = req.params.id;
-    db.query("DELETE FROM movies WHERE id = ?", [movieId], (err, result) => {
-      if (err) {
-        res.status(500).send("Error deleting the movie");
-      } else {
-        res.status(200).send("Movie deleted successfully");
-      }
-    });
+  const movieId = req.params.id;
+  db.query("DELETE FROM movies WHERE id = ?", [movieId], (err, result) => {
+    if (err) {
+      res.status(500).send("Error deleting the movie");
+    } else {
+      res.status(200).send("Movie deleted successfully");
+    }
   });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port : ${port}`);
